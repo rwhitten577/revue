c12053f369632c008e39b924bd0e3e2ac4c66cb5
Rachel Adler <adler.rachel.a@gmail.com>
2016-07-06 11:33:01 -0400

    fixed the hound errors on the Devise branch

--- db/migrate/20160706140721_devise_create_users.rb
+++ db/migrate/20160706140721_devise_create_users.rb
@@ -3,8 +3,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username, null: false
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
       t.string   :reset_password_token
