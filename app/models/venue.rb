8161b0e41558d923608a1966dfca8df105c0aebc
Rachel Adler <adler.rachel.a@gmail.com>
2016-07-07 13:31:50 -0400

    created tests for model, created model, controller, and migration

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -4,4 +4,7 @@ class Venue < ActiveRecord::Base
   validates :city, presence: true
   validates :state, presence: true, length: { is: 2 }
   validates :category, presence: true, inclusion: { in: %w(Music Comedy Poetry Theater) }
+
+  has_many :reviews
+  belongs_to :user
 end
