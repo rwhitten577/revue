76ea6cb617fa291f86c2a507f4fb64a8b5d1bbd0
Ryan Whitten <RyanWhitten@Ryans-MacBook-Pro-2.local>
2016-07-07 15:53:58 -0400

    Fixed hound spacing again

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,10 +16,10 @@ class User < ActiveRecord::Base
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-     where(conditions.to_hash).where(['lower(username) = :value OR
-       lower(email) = :value', { value: login.downcase }]).first
+      where(conditions.to_hash).where(['lower(username) = :value OR
+        lower(email) = :value', { value: login.downcase }]).first
     elsif conditions.key?(:username) || conditions.key?(:email)
-     where(conditions.to_hash).first
+      where(conditions.to_hash).first
     end
   end
 end
