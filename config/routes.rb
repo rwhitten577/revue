7213275fa516b434ac3047a4c6b18028629bb2a0
Ryan Whitten <RyanWhitten@Ryans-MacBook-Pro-2.local>
2016-07-07 17:02:51 -0400

    Added basic styling on venue index and new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'venues#index'
   resources :venues do
     resources :reviews
+  end
+
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
 end
