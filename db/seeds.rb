32e489556a4663213ab6f56ca618b4263083c949
Ryan Whitten <RyanWhitten@Ryans-MacBook-Pro-2.local>
2016-07-08 09:30:00 -0400

    Fixed hound sass violations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
-Venue.create(name: 'House of Blues', street: '15 Lansdowne St.', city: 'Boston', state: 'MA', website: 'http://www.houseofblues.com/boston/', category: 'Music', description: "Rock- & blues-themed chain with Southern dishes such as po' boys & jambalaya, plus live music.")
+Venue.create(
+  name: 'House of Blues',
+  street: '15 Lansdowne St.',
+  city: 'Boston',
+  state: 'MA',
+  website: 'http://www.houseofblues.com/boston/',
+  category: 'Music',
+  description: "Rock- & blues-themed chain with Southern dishes such as po'
+    boys & jambalaya, plus live music.")
