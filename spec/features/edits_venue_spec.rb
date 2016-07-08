cc228fe9fa0a4dcb52a02dc6d6f92c3f99f11959
Ryan Whitten <RyanWhitten@Ryans-MacBook-Pro-2.local>
2016-07-08 09:45:20 -0400

    Change before block in create and edit to context

--- spec/features/edits_venue_spec.rb
+++ spec/features/edits_venue_spec.rb
@@ -2,107 +2,109 @@ require 'rails_helper'
 
 feature 'user edits venue' do
   let(:venue) { FactoryGirl.create(:venue) }
-  let(:new_venue)  { FactoryGirl.attributes_for(:venue) }
+  let(:new_venue)  { FactoryGirl.build(:venue) }
 
-  before do |example|
-    unless example.metadata[:skip_before]
-      sign_in
+  context 'inauthenticated user' do
+    scenario 'cannot edit venue' do
+      visit venue_path(venue)
+
+      expect(page).not_to have_link('Edit Venue')
     end
   end
 
-  scenario 'inauthenticated user cannot edit venue', :skip_before do
-    visit venue_path(venue)
+  context 'authenticated user' do
+    before do
+      sign_in
+    end
 
-    expect(page).not_to have_link('Edit Venue')
-  end
+    scenario 'visits edit venue form' do
+      visit venue_path(venue)
+      click_link 'Edit Venue'
 
-  scenario 'authenticated user visits edit venue form' do
-    visit venue_path(venue)
-    click_link 'Edit Venue'
+      expect(current_path).to eq(edit_venue_path(venue))
 
-    expect(current_path).to eq(edit_venue_path(venue))
+      expect(page).to have_selector('form')
 
-    expect(page).to have_selector('form')
+      expect(page).to have_content('Venue Name')
+      expect(page).to have_content('Street')
+      expect(page).to have_content('City')
+      expect(page).to have_content('State')
+      expect(page).to have_content('Venue Website')
+      expect(page).to have_content('Venue Category')
+      expect(page).to have_content('Description')
 
-    expect(page).to have_content('Venue Name')
-    expect(page).to have_content('Street')
-    expect(page).to have_content('City')
-    expect(page).to have_content('State')
-    expect(page).to have_content('Venue Website')
-    expect(page).to have_content('Venue Category')
-    expect(page).to have_content('Description')
+      expect(find('#venue_name').value).to eq(venue[:name])
+      expect(find('#venue_street').value).to eq(venue[:street])
+      expect(find('#venue_city').value).to eq(venue[:city])
+      expect(find('#venue_website').value).to eq(venue[:website])
+      expect(find('#venue_category_music').value).to eq(venue[:category])
+      expect(find('#venue_description').value).to eq(venue[:description])
+    end
 
-    expect(find('#venue_name').value).to eq(venue[:name])
-    expect(find('#venue_street').value).to eq(venue[:street])
-    expect(find('#venue_city').value).to eq(venue[:city])
-    expect(find('#venue_website').value).to eq(venue[:website])
-    expect(find('#venue_category_music').value).to eq(venue[:category])
-    expect(find('#venue_description').value).to eq(venue[:description])
-  end
+    scenario 'inputs valid name, location, and category' do
+      visit venue_path(venue)
+      click_link 'Edit Venue'
 
-  scenario 'user inputs valid name, location, and category' do
-    visit venue_path(venue)
-    click_link 'Edit Venue'
+      fill_in 'Venue Name', with: new_venue[:name]
+      fill_in 'Street', with: new_venue[:street]
+      fill_in 'City', with: new_venue[:city]
+      select 'Massachusetts', from: 'State'
+      fill_in 'Venue Website', with: new_venue[:website]
+      choose 'venue_category_comedy'
+      fill_in 'venue_description', with: new_venue[:description]
+      click_button 'Save Changes'
 
-    fill_in 'Venue Name', with: new_venue[:name]
-    fill_in 'Street', with: new_venue[:street]
-    fill_in 'City', with: new_venue[:city]
-    select 'Massachusetts', from: 'State'
-    fill_in 'Venue Website', with: new_venue[:website]
-    choose 'venue_category_comedy'
-    fill_in 'venue_description', with: new_venue[:description]
-    click_button 'Save Changes'
+      expect(current_path).to eq(venue_path(venue))
 
-    expect(current_path).to eq(venue_path(venue))
+      expect(page).to have_content('Venue successfully saved!')
+      expect(page).to have_content(new_venue[:name])
+      expect(page).to have_content('Comedy')
 
-    expect(page).to have_content('Venue successfully saved!')
-    expect(page).to have_content(new_venue[:name])
-    expect(page).to have_content('Comedy')
+      expect(page).not_to have_content(venue[:name])
+    end
 
-    expect(page).not_to have_content(venue[:name])
-  end
+    scenario 'does not complete required fields' do
+      visit venue_path(venue)
+      click_link 'Edit Venue'
 
-  scenario 'user does not complete required fields' do
-    visit venue_path(venue)
-    click_link 'Edit Venue'
+      fill_in 'Name', with: ''
+      fill_in 'Street', with: ''
+      fill_in 'City', with: new_venue[:city]
+      select 'Massachusetts', from: 'State'
+      fill_in 'Venue Website', with: new_venue[:website]
+      choose 'venue_category_poetry'
+      fill_in 'venue_description', with: new_venue[:description]
+      click_button 'Save Changes'
 
-    fill_in 'Name', with: ''
-    fill_in 'Street', with: ''
-    fill_in 'City', with: new_venue[:city]
-    select 'Massachusetts', from: 'State'
-    fill_in 'Venue Website', with: new_venue[:website]
-    choose 'venue_category_poetry'
-    fill_in 'venue_description', with: new_venue[:description]
-    click_button 'Save Changes'
+      expect(page).to have_content("Name can't be blank")
+      expect(page).to have_content("Street can't be blank")
 
-    expect(page).to have_content("Name can't be blank")
-    expect(page).to have_content("Street can't be blank")
+      expect(page).not_to have_content('Venue successfully saved!')
 
-    expect(page).not_to have_content('Venue successfully saved!')
+      expect(page).to have_content('There were problems saving your venue')
 
-    expect(page).to have_content('There were problems saving your venue')
+      expect(find('#venue_city').value).to eq(new_venue[:city])
+      expect(find('#venue_website').value).to eq(new_venue[:website])
+      expect(find('#venue_description').value).to eq(new_venue[:description])
+    end
 
-    expect(find('#venue_city').value).to eq(new_venue[:city])
-    expect(find('#venue_website').value).to eq(new_venue[:website])
-    expect(find('#venue_description').value).to eq(new_venue[:description])
-  end
+    scenario 'submits a blank form' do
+      visit venue_path(venue)
+      click_link 'Edit Venue'
 
-  scenario 'user submits a blank form' do
-    visit venue_path(venue)
-    click_link 'Edit Venue'
+      fill_in 'Name', with: ''
+      fill_in 'Street', with: ''
+      fill_in 'City', with: ''
+      select 'Select a State', from: 'State'
+      click_button 'Save Changes'
 
-    fill_in 'Name', with: ''
-    fill_in 'Street', with: ''
-    fill_in 'City', with: ''
-    select 'Select a State', from: 'State'
-    click_button 'Save Changes'
+      expect(page).not_to have_content('Venue successfully added!')
 
-    expect(page).not_to have_content('Venue successfully added!')
-
-    expect(page).to have_content("Name can't be blank")
-    expect(page).to have_content("Street can't be blank")
-    expect(page).to have_content("City can't be blank")
-    expect(page).to have_content("State can't be blank")
-    expect(page).to have_content('There were problems saving your venue')
+      expect(page).to have_content("Name can't be blank")
+      expect(page).to have_content("Street can't be blank")
+      expect(page).to have_content("City can't be blank")
+      expect(page).to have_content("State can't be blank")
+      expect(page).to have_content('There were problems saving your venue')
+    end
   end
 end
