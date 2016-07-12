00970e80caef394f2dbfb7e38fee0bdadbc1e9de
Mike <Mike@x.local>
2016-07-12 14:37:33 -0400

    more hound

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) do
-    User.new(
-      username: 'xxxxx',
-      email: 'flvbliv',
-      encrypted_password: 'jfoeiej',
-      admin: false
+    User.new
+      (
+        username: 'xxxxx',
+        email: 'flvbliv',
+        encrypted_password: 'jfoeiej',
+        admin: false
       )
   end
 
