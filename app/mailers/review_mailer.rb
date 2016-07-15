c690cb0f5186a0509d9a83f1e6947cd0ba998a43
Mike <Mike@x.local>
2016-07-15 12:28:39 -0400

    seriously hopefully fixed mail

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -2,7 +2,7 @@ class ReviewMailer < ApplicationMailer
   def new_review(review)
     @review = review
     mail(
-      to: review.venue.user.email,
+      to: @review.venue.user.email,
       subject: "New review for #{review.venue.name}"
     )
   end
